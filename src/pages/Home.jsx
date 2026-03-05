134de8e4a558d255934605f933651cc779801c73
João Fonseca <joaofonsecan3@gmail.com>
2026-03-05 15:39:54 -0300

    refactor: melhora estrutura da página Home com elemento main

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,21 +8,23 @@ import FutureSkills from "../components/landing/FutureSkills"
 import FamilySchoolSection from "../components/landing/FamilySchoolSection"
 import CTASection from "../components/landing/CTASection"
 import Footer from "../components/landing/Footer"
-import StarField from "../components/landing/StarField"
 
 export default function Home() {
   return (
     <>
-      <StarField />
       <Header />
-      <HeroSection />
-      <UniverseSection />
-      <ProblemSection />
-      <SolutionSection />
-      <HowItWorks />
-      <FutureSkills />
-      <FamilySchoolSection />
-      <CTASection />
+
+      <main>
+        <HeroSection />
+        <UniverseSection />
+        <ProblemSection />
+        <SolutionSection />
+        <HowItWorks />
+        <FutureSkills />
+        <FamilySchoolSection />
+        <CTASection />
+      </main>
+
       <Footer />
     </>
   )
